ce3fe49aac9f67a68c0660b99e1f386f17cf8e8e
Stephen Poole <stephenwpoole@gmail.com>
2018-07-11 03:41:52 -0400

    Added tests for addons and perks.

---
+++ enum.js
@@ -0,0 +1,44 @@
+export const Amounts = {
+    SLIGHTLY: 1,
+    MODERATELY: 2,
+    CONSIDERABLY: 3,
+    TREMENDOUSLY: 4
+}
+
+export const PlayerTypes = {
+    SURVIVOR: 1,
+    KILLER: 2
+}
+
+export const TypeNames = {
+    1: 'SURVIVOR',
+    2: 'KILLER'
+}
+
+export const Rarities = {
+    COMMON: 1,
+    UNCOMMON: 2,
+    RARE: 3,
+    VERY_RARE: 4,
+    ULTRA_RARE: 5
+}
+
+export const ItemTypes = {
+    EMPTY: 'EMPTY',
+    FLASHLIGHT: 'FLASHLIGHT',
+    KEY: 'KEY',
+    MAP: 'MAP',
+    AID_KIT: 'AID_KIT',
+    TOOLBOX: 'TOOLBOX'
+}
+
+export const ModifierTypes = {
+    NONE: -1,
+    POWER: 1,
+    ITEM: 2,
+    ADDON: 3,
+    OFFERING: 4,
+    PERK: 5,
+    PLAYER: 6,
+    PLAYER_TYPE: 7
+}
---
+++ test/Helpers/Helpers.js
@@ -0,0 +1,93 @@
+import * as Enum from '../../enum'
+const killers = require('../../data/Killers.json')
+const survivors = require('../../data/Survivors.json')
+
+export class Helpers {
+    static resolve(done, value = true) {
+        if (typeof done === 'function') {
+            !!value ? done() : done(new Error())
+        }
+    }
+    static isJSON(done, value) {
+        try {
+            const j = JSON.stringify(value)
+            JSON.parse(j)
+            this.resolve(done)
+        } catch (e) {
+            console.log(e)
+            this.resolve(done, false)
+        }
+    }
+    static exists(done, value, property) {
+        this.resolve(done, property in value)
+    }
+    static isNumber(done, value) {
+        const passed = typeof value === 'number'
+        this.resolve(done, passed)
+        return passed
+    }
+    static isString(done, value) {
+        const passed = typeof value === 'string'
+        this.resolve(done, passed)
+        return passed
+    }
+    static isStringOrNumber(done, value) {
+        this.resolve(done, this.isString(undefined, value) || this.isNumber(undefined, value))
+    }
+    static unique(done, value, property, arr) {
+        this.resolve(done, !arr.filter(item => value[property] === value).length)
+    }
+    static occurrences(done, value, needle, count = 1) {
+        this.resolve(done, value.split(needle).length - 1 === count)
+    }
+    static isNotEmpty(done, value) {
+        switch (typeof value) {
+            case 'object':
+                if (Array.isArray(value)) {
+                    this.resolve(done, value.length)
+                } else {
+                    this.resolve(done, Object.keys(value).length)
+                }
+                break
+            case 'string':
+                this.resolve(done, value.length)
+                break
+            case 'number':
+                this.resolve(done)
+                break
+            default:
+                this.resolve(done, false)
+                break
+        }
+    }
+    static isLength(done, value, length) {
+        this.resolve(done, value.length === length)
+    }
+    static isArray(done, value) {
+        this.resolve(done, Array.isArray(value))
+    }
+    static isRarity(done, value) {
+        this.resolve(done, value in Enum.Rarities)
+    }
+    static isItem(done, value) {
+        this.resolve(done, value in Enum.ItemTypes)
+    }
+    static isKiller(done, value, allowGeneric = false) {
+        for (let { index } of killers) {
+            if (value === index) {
+                this.resolve(done)
+                return
+            }
+        }
+        this.resolve(done, allowGeneric ? value === 'ALL' : false)
+    }
+    static isSurvivor(done, value, allowGeneric = false) {
+        for (let { index } of survivors) {
+            if (value === index) {
+                this.resolve(done)
+                return
+            }
+        }
+        this.resolve(done, allowGeneric ? value === 'ALL' : false)
+    }
+}
