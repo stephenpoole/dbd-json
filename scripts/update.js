ad21c7198982cdbe7a9b8d5bbb2fffc0c8e9a3f1
Stephen Poole <stephenwpoole@gmail.com>
2021-06-29 11:37:28 -0400

    Steam update 5.0.2

---
+++ scripts/update.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const lastVersionPath = "./version.txt";
+const filePath =
+    "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Dead by Daylight\\DeadByDaylight\\Content\\Version\\DeadByDaylightVersionNumber.txt";
+
+fs.stat(path.resolve(__dirname, lastVersionPath), (error, stat) => {
+    let lastVersion;
+    if (stat) {
+        lastVersion = fs.readFileSync(path.resolve(__dirname, lastVersionPath)).toString();
+    }
+    const version = fs.readFileSync(filePath).toString();
+    if (lastVersion !== version) {
+        console.log(before, "=>", after);
+    } else {
+        console.log("no updates");
+    }
+
+    fs.writeFileSync(path.resolve(__dirname, lastVersionPath), version);
+});
